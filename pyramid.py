80af43a463dcc4e7820cd75283f90fedb35b74fd
Vittorio Calcagno <vc@maptasking.com>
2021-12-26 18:01:36 -0500

    finished print_pyramid() function

--- pyramid.py
+++ pyramid.py
@@ -11,13 +11,35 @@ A pyramid of height 3 would look like:
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
+def build_triangle(height):
+    
+    # This would be the maximum width of the triangle / size of the base. All lines should have this total width in number of characters
+    n_max = calculate_line(height)
+
+    for i in range(1,height+1):
+        n = calculate_line(i)
+        # A simple way to fill empty spaces in a line with our desired character
+        print(('='*n).center(n_max,'-'))
+
+def calculate_line(line_number):
+    # The number of '=' in the line can be defined by the function n = (l - 1)*2 + 1
+    n = (line_number - 1)*2 + 1
+    return n
 
 def print_pyramid(rows):
     """Print a pyramid of a given height
 
     :param int rows: total height
     """
-    raise NotImplementedError("Called with rows={}".format(rows))
+
+    try:
+        rows = int(rows)
+        return build_triangle(rows)
+    except ValueError:
+        print("--rows must be an integer")
+        return
+
+    
 
 
 if __name__ == "__main__":
